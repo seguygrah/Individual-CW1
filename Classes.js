b6f2ba25cd6343eb8b8611231f4b25630d2a153f
seguygrah <ks1682@live.mdx.ac.uk>
2023-03-09 12:10:24 +0400

    list of classes

---
+++ Classes.js
@@ -0,0 +1,49 @@
+const products = [
+    {
+      "id": 1001,
+     "title": "Mathematics Classes",
+     "description": "Provides additional support and instruction to help children develop a strong foundation in math concepts, problem-solving, and analytical skills.",
+     "price": 500,
+     "image": "img/math.jpg",
+     "availableInventory": 10,
+     "rating": 5,
+ },
+ {
+     "id": 1002,
+     "title": "Art Classes",
+     "description": "Encourages creativity and imagination through painting, drawing, and crafting.",
+     "price": 300,
+     "image": "img/Art.jpg",
+     "availableInventory": 10,
+     "rating": 5,
+ },
+ {
+     "id": 1003,
+     "title": "Music Lessons",
+     "description": "Introduces children to music theory, instruments, and singing to improve their musical abilities.",
+     "price": 300,
+     "image": "img/music.jpg",
+     "availableInventory": 10,
+     "rating": 4,
+ },
+ {
+     "id": 1004,
+     "title": "Sports Clubs",
+     "description": "Gucci Cat Leash",
+     "price": 300,
+     "image": "img/sport.png",
+     "availableInventory": 10,
+     "rating": 5,
+ },
+ {
+    "id": 1005,
+    "title": "Coding Classes",
+    "description": "Teaches kids the basics of coding and computer programming, preparing them for future technology-related careers.",
+    "price": 500,
+    "image": "img/coding.jpg",
+    "availableInventory": 10,
+    "rating": 5,
+}
+ 
+ ]
+ 
